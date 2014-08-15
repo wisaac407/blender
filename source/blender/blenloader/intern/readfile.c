3ddcb262f0e590839470fc33cd441ba4a09ab67c
Lukas Tönne <lukas.toenne@gmail.com>
2014-08-15 17:51:19 +0200

    Revert "Hair System test"
    This reverts commit 41e84700c35a602385d97bcc3e2d45031c99573f.

--- source/blender/blenloader/intern/readfile.c
+++ source/blender/blenloader/intern/readfile.c
@@ -68,7 +68,6 @@
 #include "DNA_genfile.h"
 #include "DNA_group_types.h"
 #include "DNA_gpencil_types.h"
-#include "DNA_hair_types.h"
 #include "DNA_ipo_types.h"
 #include "DNA_key_types.h"
 #include "DNA_lattice_types.h"
@@ -135,7 +134,6 @@
 #include "BKE_particle.h"
 #include "BKE_pointcache.h"
 #include "BKE_report.h"
-#include "BKE_rigidbody.h"
 #include "BKE_sca.h" // for init_actuator
 #include "BKE_scene.h"
 #include "BKE_screen.h"
@@ -4584,19 +4582,6 @@ static void direct_link_pose(FileData *fd, bPose *pose)
 	}
 }
 
-static void direct_link_hair_system(FileData *fd, HairSystem *hsys)
-{
-	HairCurve *hair;
-	int i;
-	
-	hsys->curves = newdataadr(fd, hsys->curves);
-	for (hair = hsys->curves, i = 0; i < hsys->totcurves; ++hair, ++i) {
-		hair->points = newdataadr(fd, hair->points);
-	}
-	
-	hsys->render_iter = NULL;
-}
-
 static void direct_link_modifiers(FileData *fd, ListBase *lb)
 {
 	ModifierData *md;
@@ -4856,17 +4841,6 @@ static void direct_link_modifiers(FileData *fd, ListBase *lb)
 				BLI_endian_switch_float_array(lmd->vertexco, lmd->total_verts * 3);
 			}
 			lmd->cache_system = NULL;
-		}
-		else if (md->type == eModifierType_Hair) {
-			HairModifierData *hmd = (HairModifierData *)md;
-
-			hmd->hairsys = newdataadr(fd, hmd->hairsys);
-			if (hmd->hairsys)
-				direct_link_hair_system(fd, hmd->hairsys);
-
-			hmd->solver = NULL;
-
-			hmd->debug_data = NULL;
 		}
 	}
 }
@@ -5262,8 +5236,6 @@ static void lib_link_scene(FileData *fd, Main *main)
 					rbw->constraints = newlibadr(fd, sce->id.lib, rbw->constraints);
 				if (rbw->effector_weights)
 					rbw->effector_weights->group = newlibadr(fd, sce->id.lib, rbw->effector_weights->group);
-				/* create empty mempool */
-				BKE_rigidbody_world_init_mempool(rbw);
 			}
 			
 			if (sce->nodetree) {
